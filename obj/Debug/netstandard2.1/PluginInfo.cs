f0ce348477c26eb0944c9a68c9ae71512d0f6b45
Danny-vD <dvduist@hotmail.nl>
2024-03-15 23:10:15 +0100

    Build dll

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace MonitorLabels
     {
         public const string PLUGIN_GUID = "MonitorLabels";
         public const string PLUGIN_NAME = "MonitorLabels";
-        public const string PLUGIN_VERSION = "1.3.1";
+        public const string PLUGIN_VERSION = "1.4.0";
     }
 }
